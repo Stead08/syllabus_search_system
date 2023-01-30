46bdee98d39f31b90ca6eba63aa7284426d55430
Stead08 <inomatama08@gmail.com>
2023-01-30 19:51:43 +0900

    1/30/2023 updated for heroku deploying still not good layout

--- server.ts
+++ server.ts
@@ -1,18 +1,10 @@
 import { serve } from "aleph/react-server";
-import { parse } from "std/flags/mod.ts";
+
 import { presetUno } from "@unocss/preset-uno";
 
-const DEFAULT_PORT = 8080;
-const argPort = parse(Deno.args).port;
-const port = argPort ? Number(argPort) : DEFAULT_PORT;
-if (isNaN(port)) {
-  console.error('Port is not a number.');
-  Deno.exit(1);
-}
 
 serve({
   ssr: true,
-  port: port,
   unocss: {
     presets: [presetUno()],
   },
